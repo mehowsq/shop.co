3e154a2ac7eace6739caf51d60c81a58e048c808
mehow <michalskuza5@gmail.com>
2024-09-20 19:58:21 +0200

    Createrd product detail page and basic UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,6 +9,7 @@ import ProductCategorySection from "~/components/product-category-section";
 import HeroSection from "~/components/hero-section";
 import Newsletter from "~/components/Newsletter/Newsletter";
 import Footer from "~/components/footer";
+import Products from "./products";
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,15 +21,14 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <DiscountCard />
-      <Navigation />
-      <HeroSection />
-      <BrandsCard />
-      <ProductsSection />
-      <ProductCategorySection />
-      <FeedbackSection />
-      <Newsletter />
-      <Footer />
+      <Products />
+      {/* <Navigation /> */}
+      {/* <HeroSection /> */}
+      {/* <BrandsCard /> */}
+      {/* <ProductsSection /> */}
+      {/* <ProductCategorySection /> */}
+      {/* <FeedbackSection /> */}
+      {/* <Footer /> */}
     </>
   );
 }
